9dd332a3a672ff6f7def1ec4e875203844adea27
g-buzzi <86495262+g-buzzi@users.noreply.github.com>
2021-09-08 09:54:26 -0300

    Tela abstrata e tela ingredientes

--- controladores/controlador_ingredientes.py
+++ controladores/controlador_ingredientes.py
@@ -1,3 +1,5 @@
+from telas.tela_mostra_ingrediente import TelaMostraIngrediente
+from telas.tela_lista_ingrediente import TelaListaIngrediente
 from controladores.controlador_abstrato import Controlador
 from entidades import ingrediente
 from entidades.ingrediente import Ingrediente
@@ -6,29 +8,86 @@ from telas.tela_ingrediente import TelaIngrediente
 
 class ControladorIngredientes(Controlador):
     def __init__(self, controlador_central):
-        super().__init__(TelaIngrediente(self))
+        super().__init__(TelaListaIngrediente())
         self.__ingredientes = {}
         self.__controlador_central = controlador_central
+        self.__lista = []
+        self.__pesquisa = False
 
     @property
     def ingredientes(self):
         return self.__ingredientes
 
-    def abre_tela_inicial(self):
-        switcher = {0: False, 1: self.cadastra_ingrediente, 2: self.altera_ingrediente, 3: self.remove_ingrediente, 4: self.lista_ingredientes, 5: self.pesquisa_ingrediente_por_nome}
-        opcoes = {1: "Cadastrar", 2: "Alterar", 3: "Remover", 4: "Listar", 5:"Pesquisar", 0: "Voltar"}
-        while True:
-            opcao = self.tela.mostra_opcoes(opcoes, "--------- Ingredientes ---------")
-            funcao_escolhida = switcher[opcao]
-            if funcao_escolhida:
-                funcao_escolhida()
-            else:
-                break
+    def inicia(self):
+        self.abre_tela_inicial()
 
-    def cadastra_ingrediente(self):
-        opcoes = {1: "Continuar cadastrando", 0: "Voltar"}
+    def abre_tela_inicial(self, dados = None):
+        switcher = {"cadastrar": self.cadastrar, "pesquisar": self.pesquisar, "lista_clique_duplo": self.mostrar, "listar": self.listar}
+        self.listar()
         while True:
-            dados = self.tela.cadastra_ingrediente()
+            self.tela = TelaListaIngrediente()
+            if self.__pesquisa is not False:
+                self.__lista = self.pesquisa_ingrediente_por_nome(self.__pesquisa)
+            else:
+                self.__lista = self.dados_ingredientes()
+            botao, valores = self.tela.lista_ingredientes(self.__lista, self.__pesquisa)
+            if botao == "voltar":
+                break
+            switcher[botao](valores)
+
+    def listar(self, valores = None):
+        self.__pesquisa = False
+
+    def dados_ingredientes(self):
+        dados = []
+        for ingrediente in self.__ingredientes.values():
+            dados.append([ingrediente.codigo, ingrediente.nome, ingrediente.unidade_medida, ingrediente.preco_unitario])
+        return dados
+
+    def dados_ingrediente(self, ingrediente: Ingrediente) -> dict:
+        dados = {"codigo": ingrediente.codigo, "nome": ingrediente.nome, "unidade_medida": ingrediente.unidade_medida, "preco_unitario": ingrediente.preco_unitario, "quantidade_estoque": ingrediente.quantidade_estoque}
+        return dados
+
+    def pesquisar(self, valores = None):
+        pesquisa = self.tela.pesquisar("Nome: ")
+        if pesquisa is not None:
+            dados_pesquisa = self.pesquisa_ingrediente_por_nome(pesquisa)
+            self.__pesquisa = pesquisa
+
+    def pesquisa_ingrediente_por_nome(self, pesquisa: str):
+        dados = []
+        for ingrediente in self.__ingredientes.values():
+            if pesquisa.lower() in ingrediente.nome.lower():
+                dados.append([ingrediente.codigo, ingrediente.nome, ingrediente.unidade_medida, ingrediente.preco_unitario])
+        return dados
+
+    def mostrar(self, dados):
+        try:
+            selecionado = dados["lista"][0]
+        except IndexError:
+            return
+        codigo_ingrediente = self.__lista[selecionado][0]
+        ingrediente = self.__ingredientes[codigo_ingrediente]
+        self.tela = TelaMostraIngrediente()
+        switcher = {"inicia_alteracao": self.alteracao, "remove": self.remove, "volta": False}
+        while True:
+            botao, dados = self.tela.mostra(self.dados_ingrediente(ingrediente))
+            if switcher[botao] is False:
+                break
+            else:
+                switcher[botao](ingrediente)
+                if botao == "remove":
+                    break
+
+    def remove(self, ingrediente: Ingrediente):
+        self.__ingredientes.pop(ingrediente.codigo)
+
+
+    def cadastrar(self, valores = None):
+        self.tela = TelaMostraIngrediente()
+        switcher = {"volta": False, "cadastra": True}
+        botao, dados = self.tela.cadastra()
+        if switcher[botao]:
             if dados["codigo"] not in self.__ingredientes.keys():
                 for ingrediente in self.__ingredientes.values():
                     if dados["nome"].lower() == ingrediente.nome.lower():
@@ -37,39 +96,21 @@ class ControladorIngredientes(Controlador):
                 else:
                     self.__ingredientes[dados["codigo"]] = Ingrediente(dados["codigo"], dados["nome"], dados["unidade_medida"], dados["preco_unitario"])
                     self.tela.mensagem("Ingrediente cadastrado com sucesso")
+                    self.listar()
             else:
                 self.tela.mensagem_erro("Código já em uso, tente outro código")
-            opcao = self.tela.mostra_opcoes(opcoes)
-            if opcao == 0:
-                break
 
 
-    def altera_ingrediente(self):
-        opcoes_alteracao = {1: "Alteração completa", 2: "Alterar código", 3: "Alterar nome", 4: "Alterar unidade de medida", 5: "Alterar preço", 0:"Finalizar alteração"}
-        funcoes_alteracao = {1: self.alteracao_completa, 2: self.altera_codigo, 3: self.altera_nome, 4: self.altera_unidade, 5: self.altera_preco, 0: False}
-        opcoes = {1: "Alterar outro ingrediente", 0: "Voltar"}
-        while True:
-            codigo = self.tela.altera_ingrediente()
-            try:
-                ingrediente = self.__ingredientes[codigo]
-            except KeyError:
-                self.tela.mensagem_erro("Nenhum ingrediente com este código existe")
-            else:
-                while True:
-                    self.mostra_ingrediente(ingrediente)
-                    opcao = self.tela.mostra_opcoes(opcoes_alteracao, "---- Opções de Alteração ----")
-                    funcao = funcoes_alteracao[opcao]
-                    if funcao is False:
-                        break
-                    funcao(ingrediente)
-            opcao = self.tela.mostra_opcoes(opcoes)
-            if opcao == 0:
-                break
+    def alteracao(self, ingrediente: Ingrediente):
+        dados = self.dados_ingrediente(ingrediente)
+        botao, dados = self.tela.altera(dados)
+        if botao == "volta":
+            return
+        else:
+            self.altera(ingrediente, dados)
         
 
-    def alteracao_completa(self, ingrediente: Ingrediente):
-        dados = self.dados_ingrediente(ingrediente)
-        dados = self.tela.alteracao_completa(dados)
+    def altera(self, ingrediente: Ingrediente, dados):
         if ingrediente.codigo != dados["codigo"]:
             if dados["codigo"] not in self.__ingredientes.keys():
                 self.__ingredientes.pop(ingrediente.codigo)
@@ -86,95 +127,6 @@ class ControladorIngredientes(Controlador):
         ingrediente.nome = dados["nome"]
         ingrediente.unidade_medida = dados["unidade_medida"]
         ingrediente.preco_unitario = dados["preco_unitario"] 
-        self.tela.mensagem("Alterações realizadas com sucesso") 
-
-    def altera_codigo(self, ingrediente: Ingrediente):
-        novo_codigo = self.tela.altera_codigo(ingrediente.codigo)
-        if novo_codigo != ingrediente.codigo:
-            if novo_codigo in self.__ingredientes.keys():
-                self.tela.mensagem_erro("Código em uso, as alteraçoes não serão realizadas")
-                return
-            else:
-                self.__ingredientes.pop(ingrediente.codigo)
-        self.__ingredientes[novo_codigo] = ingrediente
-        ingrediente.codigo = novo_codigo
-        self.tela.mensagem("Alterações realizadas com sucesso") 
-                
-    def altera_nome(self, ingrediente: Ingrediente):
-        novo_nome = self.tela.altera_nome(ingrediente.nome)
-        if novo_nome != ingrediente.nome:
-            for ing in self.__ingredientes.values():
-                if ing.nome.lower() == novo_nome.lower() and ing != ingrediente:
-                    self.tela.mensagem_erro("Nome duplicado, alterações não serão realizadas")
-                    return
-        ingrediente.nome = novo_nome
-        self.tela.mensagem("Alterações realizadas com sucesso") 
-
-
-    def altera_unidade(self, ingrediente: Ingrediente):
-        nova_unidade = self.tela.altera_unidade(ingrediente.unidade_medida)
-        ingrediente.unidade_medida = nova_unidade
-        self.tela.mensagem("Alterações realizadas com sucesso") 
-
-    def altera_preco(self, ingrediente: Ingrediente):
-        novo_preco = self.tela.altera_preco(ingrediente.unidade_medida, ingrediente.preco_unitario)
-        ingrediente.preco_unitario = novo_preco
-        self.tela.mensagem("Alterações realizadas com sucesso") 
-
-    def remove_ingrediente(self):
-        opcoes_remover = {1: "Remover ingrediente", 0: "Cancelar"} 
-        opcoes = {1: "Continuar removendo", 0: "Voltar"}
-        while True:
-            codigo = self.tela.remove_ingrediente()
-            try:
-                ingrediente = self.__ingredientes[codigo]
-                self.mostra_ingrediente(ingrediente)
-                opcao = self.tela.mostra_opcoes(opcoes_remover, "Remover o ingrediente?")
-                if opcao == 0:
-                    self.tela.mensagem("Remoção cancelada")
-                else:
-                    self.__controlador_central.controlador_receitas.remove_ingrediente_associado_receitas(self.__ingredientes[codigo])
-                    self.__ingredientes.pop(codigo)
-                    self.tela.mensagem("Ingrediente removido")
-            except KeyError:
-                self.tela.mensagem_erro("Nenhum ingrediente com este código existe")
-            opcao = self.tela.mostra_opcoes(opcoes)
-            if opcao == 0:
-                break
-
-    def lista_ingredientes(self):
-        self.tela.cabecalho("Listar Ingredientes")
-        for ingrediente in self.__ingredientes.values():
-            self.mostra_ingrediente(ingrediente)
-
-    def pesquisa_ingrediente_por_nome(self):
-        opcoes = {1: "Continuar pesquisa", 0: "Voltar"}
-        while True:
-            pesquisa = self.tela.pesquisa_ingrediente_por_nome()
-            resultados = False
-            for ingrediente in self.__ingredientes.values():
-                if pesquisa in ingrediente.nome.lower():
-                    resultados = True
-                    self.mostra_ingrediente(ingrediente)
-            if not resultados:
-                self.tela.mensagem("Nenhum ingrediente com esse nome foi encontrado") 
-            opcao = self.tela.mostra_opcoes(opcoes)
-            if opcao == 0:
-                break
-
-    def dados_ingrediente(self, ingrediente: Ingrediente) -> dict:
-        dados = {"codigo": ingrediente.codigo, "nome": ingrediente.nome, "unidade_medida": ingrediente.unidade_medida, "preco_unitario": ingrediente.preco_unitario, "quantidade_estoque": ingrediente.quantidade_estoque}
-        return dados
-
-    def mostra_ingrediente(self, ingrediente: Ingrediente):
-        dados = self.dados_ingrediente(ingrediente)
-        self.tela.mostra_ingrediente(dados)
-
-    def seleciona_ingrediente_por_codigo(self, codigo: int) -> Ingrediente:
-        try:
-            ingrediente = self.__ingredientes[codigo]
-            return ingrediente
-        except KeyError:
-            return False
+        self.tela.mensagem("Alterações realizadas com sucesso")
 
 
--- telas/tela_abstrata.py
+++ telas/tela_abstrata.py
@@ -1,23 +1,137 @@
 from abc import ABC, abstractmethod
+from tkinter.constants import CENTER, LEFT
+import PySimpleGUI as sg
 
 
 class Tela(ABC):
     @abstractmethod
-    def __init__(self, controlador):
-        self.__controlador = controlador
+    def __init__(self):
+        self.__window = None
+        self.definir_tema()
 
     @property
     def controlador(self):
         return self.__controlador
 
-    def mostra_opcoes(self, opcoes: dict, titulo = "--------- Opções ---------") -> int:
-        print(titulo)
-        for numero, opcao in opcoes.items():
-            print("{}: {}".format(numero, opcao))
-        print()
-        opcao = self.le_num_inteiro("Opção: ", opcoes.keys())
-        print()
-        return opcao
+    @property
+    def window(self):
+        return self.__window
+
+    @window.setter
+    def window(self, window):
+        self.__window = window
+
+    def close(self):
+        if self.__window is not None:
+            self.__window.close()
+
+#======================================= Utilidades =============================================
+
+    def definir_tema(self):
+        tema =  {"BACKGROUND": "#FFEDB7", 
+         "TEXT": "#2A211C",
+         "INPUT": "#FCAA7D", 
+         "TEXT_INPUT": "#3A312C", 
+         "SCROLL": "#3A312C", 
+         "BUTTON": ("#FC9326","#3A312C"), 
+         "PROGRESS": ("#FC9326","#3A312C"),
+         "BORDER": 1, 
+         "SLIDER_DEPTH": 0, 
+         "PROGRESS_DEPTH": 0}
+        sg.theme_add_new("Padaria", tema)
+        sg.theme("Padaria")
+
+    def janela(self, layout, titulo = "Padaria Elsecall", background = None):
+        janela = sg.Window(titulo, layout= layout, margins=(0,0), finalize= True, element_justification = LEFT, use_custom_titlebar= True, background_color= background)
+        return janela
+
+    def titulo(self, texto, font_size = 14):
+        titulo = sg.Text(texto, background_color="#3A312C",
+                        text_color="#FC9326", pad=(0,0), 
+                        expand_x = True, justification="center", 
+                        font="Arial {} bold".format(font_size), 
+                        relief= sg.RELIEF_FLAT, border_width=10)
+        return titulo
+
+    def botao(self, texto, chave, desativado = False, expand_x = False, expand_y = False, padding = None):
+        if desativado:
+            botao = sg.Button(button_text = texto, key = chave, 
+                              disabled=True, button_color=("#3A312C", "#FC9326"), 
+                              disabled_button_color= ("#3A312C", "#3A312C"),
+                              expand_x= expand_x, expand_y= expand_y,
+                              pad = padding)
+        else:
+            botao = sg.Button(button_text = texto, key = chave,
+                              expand_x= expand_x, expand_y= expand_y,
+                              pad = padding)
+        return botao
+
+    def label(self, texto = "", tamanho = (None, None)):
+        label = sg.Text(texto, font="Arial 10 bold", size= tamanho, justification= LEFT)
+        return label
+
+    def entrada(self, chave, valor = "", leitura = False, tamanho = (50, 1)):
+        entrada = sg.InputText(default_text = valor, key= chave, size= tamanho,  readonly = leitura, disabled_readonly_background_color= "#FFEDB7", disabled_readonly_text_color= "#3A312C", border_width=0)
+        return entrada
+
+    def textarea(self, chave, valor = "", leitura = False, tamanho = (55, 10)):
+        if leitura:
+            area = sg.Text(valor, size= tamanho, key= chave, border_width= 0.5, relief= sg.RELIEF_SOLID)
+        else:
+            area = sg.Multiline(key = chave, default_text = valor, size= tamanho)
+        return area
+
+    def seletor(self, chave, valores = [], valor_selecionado = None, tamanho = (None, None), leitura = False):
+        if leitura:
+            seletor = sg.Text(valor_selecionado, size= tamanho, key= chave, border_width= 0)
+        else:
+            seletor = sg.Combo(valores, valor_selecionado, key= chave, size = tamanho, disabled= leitura)
+        return seletor
+
+    def radio(id_grupo, texto, chave, selecionado = False, tamanho = (None, None), leitura = False):
+        radio = sg.Radio(texto, id_grupo, key = id_grupo + "_" + chave, default = selecionado, size = tamanho, circle_color= "#FCAA7D", disabled= leitura) 
+        return radio
+
+#======================================= Bases pra tela =============================================
+
+    def opcoes(self, botoes = {}, selecionado = ""):
+        opcoes = []
+        for nome, chave in botoes.items():
+            if chave == selecionado:
+                bt = self.botao(nome, chave= chave, expand_x=True,
+                                padding=((0.5, 0.5), (0.5, 0.5)), 
+                                desativado= True)
+            else:
+                bt = self.botao(nome, chave= chave, expand_x=True,
+                                padding=((0.5, 0.5), (0.5, 0.5)))
+            opcoes.append(bt)
+        return opcoes
+
+    def pesquisar(self, texto = "Pesquisa: "):
+        pesquisa = sg.popup_get_text(texto, title = "Pesquisa")
+        return pesquisa
+
+    def lista(self, heading = [], valores = [], chave = "lista", auto_size = True):
+        if len(valores) == 0:
+            auto_size = False
+        tabela = sg.Table(valores, heading, key= chave,
+                          expand_x = True, expand_y= True,
+                          header_text_color= "#FC9326",
+                          header_background_color= "#3A312C",
+                          pad=(0,0), justification= CENTER,
+                          auto_size_columns= auto_size)
+        return tabela
+
+    def configura_lista(self, chave_lista = "lista"):
+        self.__window[chave_lista].bind('<Double-1>', "_clique_duplo")
+
+    def mensagem(self, mensagem: str):
+        sg.popup(mensagem, title = "Mensagem")
+
+    def mensagem_erro(self, mensagem: str):
+        sg.popup_error(mensagem, title = "Error")
+
+""" Fazer novas checagens
 
     def le_num_inteiro(self, mensagem: str = "Escolha uma opção: ", valores_validos: list = None) -> int:
         while True:
@@ -47,21 +161,4 @@ class Tela(ABC):
         string = input(mensagem)
         return string.strip()
 
-    def cabecalho(self, mensagem: str):
-        print("-------- " + mensagem + " --------")
-        print()
-
-    def mensagem_erro(self, mensagem: str):
-        print()
-        print("### ERRO ###")
-        print(mensagem)
-        print()
-
-    def mensagem(self, mensagem: str):
-        print("- {} -".format(mensagem))
-        print()
-
-    def quebra_linha(self):
-        print()
-
-
+"""
